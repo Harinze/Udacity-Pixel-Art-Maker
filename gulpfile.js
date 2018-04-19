ec3c9d6feca78afeabff9643248b6d7e09abb9f8
Asjas <asjas@outlook.com>
2018-04-19 19:28:44 +0200

    add js gulp task and minify js code

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
 const cleanCSS = require('gulp-clean-css');
 const del = require('del');
 const gulp = require('gulp');
@@ -6,6 +7,7 @@ const livereload = require('gulp-livereload');
 const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
 
 // HTML
 gulp.task('reload-html', () => {
@@ -16,12 +18,12 @@ gulp.task('reload-html', () => {
 gulp.task('styles', () =>
   gulp
     .src('./css/styles.css')
-    .pipe(sourcemaps.init())
     .pipe(plumber(function (err) {
       console.log('Styles Task Error');
       console.log(err);
       this.emit('end');
     }))
+    .pipe(sourcemaps.init())
     .pipe(autoprefixer())
     .pipe(cleanCSS({ compatibility: 'ie11' }))
     .pipe(cleanCSS({ level: '2' }))
@@ -34,8 +36,31 @@ gulp.task('styles', () =>
     .pipe(gulp.dest('./css/'))
     .pipe(livereload()));
 
+// JavaScript
+gulp.task('minify-js', () =>
+  gulp
+    .src('./js/designs.js')
+    .pipe(plumber(function (err) {
+      console.log('JavaScript Task Error');
+      console.log(err);
+      this.emit('end');
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015'],
+    }))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('./', {
+      sourceMappingURL(file) {
+        return `${file.relative}.map`;
+      },
+    }))
+    .pipe(gulp.dest('./js/'))
+    .pipe(livereload()));
+
 // Build task
-gulp.task('build', ['styles'], () => {
+gulp.task('build', ['styles', 'minify-js'], () => {
   console.log('Building CSS Styles.');
 });
 
@@ -45,5 +70,6 @@ gulp.task('watch', ['build'], () => {
   require('./server.js');
   livereload.listen();
   gulp.watch('index.html', ['reload-html']),
-  gulp.watch('css/styles.css', ['styles']);
+  gulp.watch('css/styles.css', ['styles']),
+  gulp.watch('js/designs.js', ['minify-js']);
 });
--- js/designs.js
+++ js/designs.js
@@ -7,19 +7,34 @@ const gridWidth = document.querySelector('#inputWidth');
 const gridCanvas = document.querySelector('#pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
   // Your code goes here!
-  // let fragment = document.createDocumentFragment();
-  // for(let x = 0; x < gridHeight; x++) {
-  //   for (let j = 0; j < gridWidth; j++) {
+  gridCanvas.innerHTML = '';
 
-  //   }
-  // }
-  console.log(gridHeight.value, gridWidth.value);
-}
+  // Create fragment to build the rows and columns on
+  const fragment = document.createDocumentFragment();
+
+  for (let x = 0; x < gridHeight.value; x++) {
+    const tr = document.createElement('tr');
+
+    for (let y = 0; y < gridWidth.value; y++) {
+      const td = document.createElement('td');
+      tr.appendChild(td);
+    }
+
+    tr.addEventListener('click', boxClicked);
+    fragment.appendChild(tr);
+  }
+  // Push fragment onto DOM
+  gridCanvas.appendChild(fragment);
+};
 
 document.querySelector('form').addEventListener('submit', function(event){
   event.preventDefault();
   makeGrid();
-})
+});
+
+function boxClicked(event) {
+  const color = colorInput.value;
+  event.target.style.backgroundColor = color;
+}
